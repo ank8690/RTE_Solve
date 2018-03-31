dac6831d60517630a23f1d826391566c82351108
ank8690 <ank.8690@gmail.com>
2018-03-31 18:50:07 +0530

    All the files in the folder are added now

---
+++ face.C
@@ -0,0 +1,60 @@
+#include"face.H"
+
+/*****************************Constructor Definitions*************************/
+Face::Face()
+{
+	//m_l_nbCells = 0;
+	m_l_FaceIndex = 0;
+	m_l_nbRays = 0;
+	//m_l_vec_FaceIndicies.clear();
+	m_d_vec_face_ray_Inten.clear();
+	/*if(m_d_vec_vec_face_ray_Inten.size())
+	{
+		for(int i = 0; i < m_d_vec_vec_face_ray_Inten.size(); ++i)
+		{
+			m_d_vec_vec_face_ray_Inten[i].clear();
+		}
+		m_d_vec_vec_face_ray_Inten.clear();
+	}*/
+}
+
+/*****************************************************************************/
+/*Cell::Cell(long l_nbCells)
+{
+	m_l_nbCells = l_nbCells;
+	m_l_cellIndex = 0;
+	m_l_nbRays = 0;
+	m_l_vec_FaceIndicies.clear();
+	if(m_d_vec_vec_cell_ray_Inten.size())
+	{
+		for(int i = 0; i < m_d_vec_vec_cell_ray_Inten.size(); ++i)
+		{
+			m_d_vec_vec_cell_ray_Inten[i].clear();
+		}
+		m_d_vec_vec_cell_ray_Inten.clear();
+	}
+}
+*/
+
+/*****************************************************************************/
+Face::~Face()
+{
+	//m_l_nbCells = 0;
+	m_l_FaceIndex = 0;
+	m_l_nbRays = 0;
+	m_d_vec_face_ray_Inten.clear();
+	//m_l_vec_FaceIndicies.clear();
+	/*if(m_d_vec_vec_face_ray_Inten.size())
+	{
+		for(int i = 0; i < m_d_vec_vec_face_ray_Inten.size(); ++i)
+		{
+			m_d_vec_vec_face_ray_Inten[i].clear();
+		}
+		m_d_vec_vec_face_ray_Inten.clear();
+	}*/
+}
+
+int main()
+{
+	return 1; 
+}
